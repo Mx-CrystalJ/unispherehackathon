05e94d2c535545bad229a004b278edaa8bfc57c3
Mx-CrystalJ <mxcrystaljohnson@gmail.com>
2025-04-02 23:47:37 +0100

    Edit allowed host and trust origin in settings for vs code.

--- unisphere/settings.py
+++ unisphere/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = 'django-insecure-(ld$yai6#f7&@4o2bxi=_fh@w-3mhrja#9&o!30@a8neh2&*zq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-joeht112-unisphere-kz4ytwskpb1.ws.codeinstitute-ide.net',
-                 '8000-joeht112-unisphere-wvpvvjl2shy.ws.codeinstitute-ide.net',
-                 '8000-joeht112-unisphere-v6m329lqdec.ws.codeinstitute-ide.net',
-                 '.herokuapp.com',]
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    '.herokuapp.com',
+]
 
 
 # Application definition
@@ -78,7 +78,7 @@ MIDDLEWARE = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeinstitute-ide.net", "https://*.herokuapp.com"
+    "https://localhost", "https://*.herokuapp.com"
 ]
 
 ROOT_URLCONF = 'unisphere.urls'
